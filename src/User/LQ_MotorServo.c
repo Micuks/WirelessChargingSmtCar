89f5a4f8700cfacf8de8f726d61b13767285ca53
Micuks <sdkjwql@gmail.com>
2022-07-20 18:20:46 +0800

    overtaken motor problem

--- src/User/LQ_MotorServo.c
+++ src/User/LQ_MotorServo.c
@@ -88,19 +88,19 @@ void EncInit(void) {
 #ifdef USE7843or7971
 void MotorCtrl(sint32 motor1, sint32 motor2) {
     if (motor1 > 0) {
-        ATOM_PWM_SetDuty(MOTOR1_P, motor1, MOTOR_FREQUENCY);
-        IfxPort_setPinLow(&MODULE_P32, 4);
+        ATOM_PWM_SetDuty(MOTOR3_P, motor1, MOTOR_FREQUENCY);
+        IfxPort_setPinLow(&MODULE_P21, 5);
     } else {
-        ATOM_PWM_SetDuty(MOTOR1_P, 10000 + motor1, MOTOR_FREQUENCY);
-        IfxPort_setPinHigh(&MODULE_P32, 4);
+        ATOM_PWM_SetDuty(MOTOR3_P, 10000 + motor1, MOTOR_FREQUENCY);
+        IfxPort_setPinHigh(&MODULE_P21, 5);
     }
 
     if (motor2 > 0) {
-        ATOM_PWM_SetDuty(MOTOR2_P, motor2, MOTOR_FREQUENCY);
-        IfxPort_setPinLow(&MODULE_P22, 3);
+        ATOM_PWM_SetDuty(MOTOR4_P, motor2, MOTOR_FREQUENCY);
+        IfxPort_setPinLow(&MODULE_P21, 3);
     } else {
-        ATOM_PWM_SetDuty(MOTOR2_P, 10000 + motor2, MOTOR_FREQUENCY);
-        IfxPort_setPinHigh(&MODULE_P22, 3);
+        ATOM_PWM_SetDuty(MOTOR4_P, 10000 + motor2, MOTOR_FREQUENCY);
+        IfxPort_setPinHigh(&MODULE_P21, 3);
     }
 }
 void MotorCtrl4w(sint32 motor1, sint32 motor2, sint32 motor3, sint32 motor4) {
